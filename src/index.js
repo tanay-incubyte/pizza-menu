6a45d483fef7d76c823178c62b48c798b96677b6
Tanay <tanay@incubyte.co>
2025-03-31 16:26:18 +0530

    final commit for pizza app

--- src/index.js
+++ src/index.js
@@ -73,12 +73,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2> Our Menu</h2>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Itaian cuisine. 6 creative dishes to choose from. All from
+            our stove oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later.</p>
       )}
@@ -86,17 +93,18 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizzaObj.soldOut) {
-    return null;
-  }
+function Pizza({ pizzaObj }) {
   return (
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>${props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        {pizzaObj.soldOut ? (
+          <span className="sold-out">SOLD OUT</span>
+        ) : (
+          <span>${pizzaData.price}</span>
+        )}
       </div>
     </li>
   );
@@ -128,11 +136,12 @@ function Footer() {
   );
 }
 
-function Order(props) {
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
       <p>
-        We're open until {props.closeHour}:00. Come visit us or order online
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online
       </p>
       <button className="btn">Order</button>
     </div>
